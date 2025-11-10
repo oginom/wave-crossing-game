65928347d740483e8d639e16a1412c7127ba49de
Masahiro Ogino <oginomaa@gmail.com>
2025-11-10 23:58:02 +0900

    setup empty scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup)
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
 }
